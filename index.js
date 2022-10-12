6aae945f9a2de7f662e29cef36d8a5ad8bf65087
rahul <rcchoudharyrahul017@gmail.com>
2022-10-12 19:25:04 +0530

    save

--- index.js
+++ index.js
@@ -1,24 +1,7 @@
 const express = require("express");
 const app = express();
 const bodyParser = require('body-parser')
-
-// connect to db
-const mongoose = require("mongoose");
-var mongoDB='mongodb://127.0.0.1/db';
-mongoose.connect(mongoDB);
-
-mongoose.Promise=global.Promise;
-
-var db =mongoose.connection;
-
-db.on('connected',function(){ 
-    console.log('Mongoose connection done');
-});
-
-
-db.on('error',function(err){
-    console.log('Mongoose default connection error: ' +err);
-});
+require('./database/db');
 
 
 // Import routes
